b9b6d2fdebd18866a66b153bf9642c13340b5b83
Archit Shukla <architshukla93@gmail.com>
2024-03-08 14:23:26 +0530

    First commit from eclipse IDE

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+	
+	public static void main(String args[]) {
+		
+		String hungry = "No";
+		
+		if (hungry=="yes") {
+			System.out.println("Eat something");
+		}
+		else {
+			System.out.println("Do not eat....");
+		}
+	}
+
+}
